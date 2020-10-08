7f3e81be7ce87c3ecb32ee0ab7dc878ffd3602bb
hidayatul-rk <71365495+hidayatul-rk@users.noreply.github.com>
2020-10-08 10:43:06 +0700

    Add files via upload

---
+++ tugas5_2.php
@@ -0,0 +1,23 @@
+<h1>OPERATOR STRING</h1>
+<?php
+
+
+echo "<table border=1>
+<tr>
+    <td>Input 1</td>
+    <td>Input 2</td>
+    <td>Hasil</td> 
+ 
+</tr>    
+<tr>
+    <td>" . "Ayo" . "</td>
+    <td>" . "Belajar" . "</td>
+    <td>" . "Ayo" . " Belajar" . "</td> 
+</tr>
+<tr>
+    <td>" . "Bersama" . "</td>
+    <td>" . "Niomic" . "</td>
+    <td>" . "Bersama" . " Niomic" . "</td>  
+</table>
+";
+printf("<br>Gabungan : " . "Ayo " . "Belajar " . "Bersama " . "Niomic :)");
